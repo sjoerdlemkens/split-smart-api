553580229646038fe094b3954b149c057ce4f859
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-09-12 11:49:00 +0200

    feat: inject create user provider in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserProvider } from './providers/create-user.provider';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly createUserProvider: CreateUserProvider) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.createUserProvider.create(createUserDto);
+  }
+}
